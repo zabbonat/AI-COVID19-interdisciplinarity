24c2ad4a0a6fdcc495a45464560c42b525e243cf
zabbonat <43636578+zabbonat@users.noreply.github.com>
2024-03-04 14:35:20 +0100

    Add files via upload

--- CODE/rEconometrics.R
+++ CODE/rEconometrics.R
@@ -10,26 +10,83 @@ library(leaps)
 library(readr)
 library(tidyr)
 library(plotly)
+library(tibble)
+
+
+df <- read_csv("data_main.csv")
+names(df) #name of columns
+df = df %>% mutate(altmetrics_score_2024_ln = log(altmetrics_score_2024+1),
+                      ai_collaborator = ifelse(score_group > 0, 1, 0),
+                      Dominant_Topic = as.factor(Dominant_Topic))
 
 
 
-# Replace NA in citations with observable values 
-df <- df %>% mutate(count_in_update_2k22 = ifelse(is.na(count_in_update_2k22), count_in, count_in_update_2k22))
-df <- df %>% mutate(ai_collaborator = ifelse(score_group > 0, 1, 0))
+# Citation and Attention --------------------------------------------------
 
 
-#---- AGGREGATE -----
 
-mod1 <- glm.nb(count_in_update_2k22 ~  score_group + reference_ai_score_mean + pmi_distance_author_3_recent + pmi_distance_reference + ai_collaborator + top_ai + sum_infl_cit_autho_ln + year_work_ln + num_unique_country_ln + count_out_ln + month_year + Dominant_Topic, data = df)
-mod2 <- lm(altmetrics_score_ln ~       score_group + reference_ai_score_mean + pmi_distance_author_3_recent + pmi_distance_reference + ai_collaborator + top_ai + sum_infl_cit_autho_ln + year_work_ln + num_unique_country_ln + count_out_ln + month_year + Dominant_Topic, data = df)
-mod3 <- lm(pmi_distance_citation ~     score_group + reference_ai_score_mean + pmi_distance_author_3_recent + pmi_distance_reference + ai_collaborator + top_ai + sum_infl_cit_autho_ln + year_work_ln + num_unique_country_ln + count_out_ln + month_year + Dominant_Topic, data = df)
+mod2B_JC_citation <- glm.nb(citation_2024_01 ~  score_group + reference_ai_score_mean + Balance_author_journal_concept + 
+                              + Balance_reference_concept  + ai_collaborator + top_ai + sum_infl_cit_autho_ln + year_work_ln 
+                            + num_unique_country_ln + count_out_ln + month_year + Dominant_Topic, 
+                            data = df)
+a = mod2B_JC_citation
 
-stargazer(mod1, mod2, mod3,
-          dep.var.labels=c("Nb. Citations", "Attention Score (log)", "Spread Score (log)"),
-          covariate.labels = c("mu_{AI}^{team}", "mu_{AI}^{kn}",
-                               "mu_{GENERAL}^{team}", "mu_{GENERAL}^{kn}",
-                               "AI collaborator", "Top AI guy",  
-                               "Past Impact (log)", "Academic Age (log)", "Nb. Countries (log)", "Nb. References (log)"),
+#########
+mod2B_JC_attention <- lm(altmetrics_score_2024_ln ~  score_group + reference_ai_score_mean + Balance_author_journal_concept 
+                         + Balance_reference_concept + ai_collaborator + top_ai + sum_infl_cit_autho_ln + year_work_ln 
+                         + num_unique_country_ln + count_out_ln + month_year + Dominant_Topic, 
+                         data = df)
+
+b = mod2B_JC_attention
+
+#########
+mod2D_JC_citation <- glm.nb(citation_2024_01 ~  score_group + reference_ai_score_mean + Disparity_author_journal_concept 
+                            + Disparity_reference_concept + ai_collaborator + top_ai + sum_infl_cit_autho_ln + year_work_ln 
+                            + num_unique_country_ln + count_out_ln + month_year + Dominant_Topic, 
+                            data = df)
+c = mod2D_JC_citation
+
+#########
+mod2D_JC_attention <- lm(altmetrics_score_2024_ln ~  score_group + reference_ai_score_mean + Disparity_author_journal_concept 
+                         + Disparity_reference_concept + ai_collaborator + top_ai + sum_infl_cit_autho_ln + year_work_ln 
+                         + num_unique_country_ln + count_out_ln + month_year + Dominant_Topic, 
+                         data = df)
+d = mod2D_JC_attention
+
+#########
+mod2V_JC_citation <- glm.nb(citation_2024_01 ~  score_group + reference_ai_score_mean + Variety_author_journal_concept 
+                            + Variety_reference_concept + ai_collaborator + top_ai + sum_infl_cit_autho_ln + year_work_ln 
+                            + num_unique_country_ln + count_out_ln + month_year + Dominant_Topic, 
+                            data = df)
+e = mod2V_JC_citation
+
+#########
+mod2V_JC_attention <- lm(altmetrics_score_2024_ln ~  score_group + reference_ai_score_mean + Variety_author_journal_concept 
+                         + Variety_reference_concept + ai_collaborator + top_ai + sum_infl_cit_autho_ln + year_work_ln 
+                         + num_unique_country_ln + count_out_ln + month_year + Dominant_Topic, 
+                         data = df)
+f = mod2V_JC_attention
+
+#########
+
+mod2S_JC_citation <- glm.nb(citation_2024_01 ~  score_group + reference_ai_score_mean + pmi_distance_2024_AUTORI_concept 
+                            + pmi_distance_2024_reference_concept + ai_collaborator + top_ai + sum_infl_cit_autho_ln + year_work_ln 
+                            + num_unique_country_ln + count_out_ln + month_year + Dominant_Topic, 
+                            data = df)
+g = mod2S_JC_citation
+
+#########
+
+mod2S_JC_attention <- lm(altmetrics_score_2024_ln ~  score_group + reference_ai_score_mean + pmi_distance_2024_AUTORI_concept 
+                         + pmi_distance_2024_reference_concept + ai_collaborator + top_ai + sum_infl_cit_autho_ln + year_work_ln 
+                         + num_unique_country_ln + count_out_ln + month_year + Dominant_Topic, 
+                         data = df)
+h = mod2S_JC_attention
+
+
+
+stargazer(a,c,e,g, b,d,f,h,
+          dep.var.labels=c("Nb. Citations","","","Attention Score", "", "" ),
           omit = c("month_year", "Dominant_Topic", "Constant"),
           align=TRUE,
           type="text", 
@@ -37,4 +94,34 @@ stargazer(mod1, mod2, mod3,
 
 
 
+# Interd. Spread ----------------------------------------------------------
 
+
+
+spread_B <- lm(pmi_distance_2024_citation_concept ~  score_group + reference_ai_score_mean + Balance_author_journal_concept 
+               + Balance_reference_concept + ai_collaborator + top_ai + sum_infl_cit_autho_ln + year_work_ln 
+               + num_unique_country_ln + count_out_ln + month_year + Dominant_Topic, 
+               data = df)
+
+spread_D <- lm(pmi_distance_2024_citation_concept ~  score_group + reference_ai_score_mean + Disparity_author_journal_concept 
+               + Disparity_reference_concept + ai_collaborator + top_ai + sum_infl_cit_autho_ln + year_work_ln 
+               + num_unique_country_ln + count_out_ln + month_year + Dominant_Topic, 
+               data = df)
+
+spread_V <- lm(pmi_distance_2024_citation_concept ~  score_group + reference_ai_score_mean + Variety_author_journal_concept 
+               + Variety_reference_concept + ai_collaborator + top_ai + sum_infl_cit_autho_ln + year_work_ln 
+               + num_unique_country_ln + count_out_ln + month_year + Dominant_Topic, 
+               data = df)
+
+spread_P <- lm(pmi_distance_2024_citation_concept ~  score_group + reference_ai_score_mean + pmi_distance_2024_AUTORI_concept 
+               + pmi_distance_2024_reference_concept + ai_collaborator + top_ai + sum_infl_cit_autho_ln + year_work_ln 
+               + num_unique_country_ln + count_out_ln + month_year + Dominant_Topic, 
+               data = df)
+
+
+stargazer(spread_B, spread_D, spread_V, spread_P,
+          dep.var.labels=c("Interd. Spread",'',''),
+          omit = c("month_year", "Dominant_Topic", "Constant"),
+          align=TRUE,
+          type="text", 
+          df = FALSE)
